f669dccaad6505ce1b54d541085ec5c67922bc6d
kzhrk <info@kzhrk.com>
2021-12-31 17:12:27 +0900

    feat: handling Invalid Input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,11 +13,18 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
+
     io::stdin()
       .read_line(&mut guess)
       .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Invalid number.");
+        continue;
+      }
+    };
 
     println!("You guessed: {}", guess);
 
